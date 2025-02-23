ff3d2e1fe3141e1dc765194c22672bf7bb71eb66
gingerbread77 <1516519317@qq.com>
2025-02-24 09:54:30 +1300

    Add footer and set up page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <>
-      <Home />
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
     </>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import MovieCards from "../../components/MovieCards/MovieCards";
+import Footer from "../../components/Footer/Footer";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
@@ -40,6 +41,7 @@ const Home = () => {
         <MovieCards section={"Upcoming"} />
         <MovieCards section={"Top Pics for you"} />
       </div>
+    <Footer/>
     </div>
   );
 };
